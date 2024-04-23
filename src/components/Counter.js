4c77546e326090f905139803216627448f21a6fc
pottabathinivivekananda <vivekananda.99666@gmail.com>
2024-04-23 17:11:32 +0530

    assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+
+
 
 function Counter() {
+	const [count, setCount] = useState(0);
+	useEffect(() => {
+		document.title = `Count: ${count}`;
+	  }, [count]);
+	const increment = () => {
+		setCount(c=>c+1);
+	};
+	const decrement = () => {
+		setCount(c=>c-1);
+	};
+	const reset = () => {
+		setCount(0);
+  	};
+
+
+
 	return (
 		<div>
 			<h1>Count: </h1>
