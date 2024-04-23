81b93cf8e9c1597a3a26916cd823987e76588c04
pottabathinivivekananda <vivekananda.99666@gmail.com>
2024-04-23 17:14:15 +0530

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,32 @@
-import React from "react";
-import { useState, useEffect } from "react";
-
-
-
+import React, { useState, useEffect } from "react";
 
 function Counter() {
-	const [count, setCount] = useState(0);
-	useEffect(() => {
-		document.title = `Count: ${count}`;
-	  }, [count]);
-	const increment = () => {
-		setCount(c=>c+1);
-	};
-	const decrement = () => {
-		setCount(c=>c-1);
-	};
-	const reset = () => {
-		setCount(0);
-  	};
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
 
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
 
-	return (
-		<div>
-			<h1>Count: </h1>
-			<button onClick={increment}>+</button>
-			<button onClick={decrement}>-</button>
-			<button onClick={reset}>Reset</button>
-		</div>
-	);
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
 }
 
 export default Counter;
